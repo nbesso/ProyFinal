b5154d5a9d6a407ff2cb4844cc5f475ff03e916d
Gonzalo Monguillot <gon_lab@hotmail.com>
2022-02-28 17:04:10 -0300

    modificacion 28-02

--- ProyectoFinal/blog/urls.py
+++ ProyectoFinal/blog/urls.py
@@ -3,4 +3,9 @@ from blog.views import *
 
 urlpatterns = [
     path('inicio/', inicio, name="Inicio"),
+    path('contacto/list', ContactoList.as_view(), name = 'List'),
+    path('nuevo/', Creacion_contacto.as_view(), name='New'),
+    path('<int:pk>/', Detalle_contacto.as_view(), name='Detail'),
+    path('editar/<int:pk>/', Modifica_contacto.as_view(), name='Edit'),
+    path('borrar/<int:pk>/', Elimina_contacto.as_view(), name='Delete'),
 ]
