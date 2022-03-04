9dd776b65227059a034abbd264644943faaa6263
Gonzalo Monguillot <gon_lab@hotmail.com>
2022-03-04 20:54:46 -0300

    20:54

--- ProyectoFinal/ProyectoFinal/settings.py
+++ ProyectoFinal/ProyectoFinal/settings.py
@@ -126,3 +126,8 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Ruta de imagenes 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
--- ProyectoFinal/ProyectoFinal/urls.py
+++ ProyectoFinal/ProyectoFinal/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 #from .views import login_request
 
 urlpatterns = [
@@ -22,3 +25,5 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('applog/', include('AppLog.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA.ROOT)
